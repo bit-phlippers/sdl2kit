89864a6b35bf5556db9e0f7bdab0472038c60343
Justin Patera <serialhex@gmail.com>
2016-06-13 10:25:15 -0400

    added code for 2d-window generation

--- src/window.lisp
+++ src/window.lisp
@@ -209,6 +209,30 @@ a cons, e.g. `(10 . 10)`."
   #+nil
   (format *error-output* "Unhandled event: ~S~%" (sdl2:get-event-type event)))
 
+;;;;;;  2D-WINDOW
+
+(defclass 2d-window (window)
+  ((renderer :initform nil :reader renderer)))
+
+(defmethod initialize-window progn ((window 2d-window) &key &allow-other-keys)
+  (with-slots (renderer sdl-window) window
+    (setf renderer (sdl2:create-renderer sdl-window))))
+
+(defmethod render :before ((window 2d-window))
+  (with-slots (renderer) window
+    (sdl2-ffi.functions:sdl-render-clear renderer)))
+
+(defmethod render :after ((window 2d-window))
+  (with-slots (renderer) window
+    (sdl2:render-present renderer)))
+
+(defmethod close-window ((window 2d-window))
+  (with-slots (renderer) window
+    (when (and (slot-boundp window 'renderer) renderer)
+      (sdl2:destroy-renderer renderer)))
+  (call-next-method))
+
+
 ;;;; GL-WINDOW
 
 (defmethod initialize-window progn
